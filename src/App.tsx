e37b20e14022af75a0ec0a27f17a2d410add02df
SeokWooLee <swlee@softnet.co.kr>
2024-04-26 10:22:04 +0900

    router outlet 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import { Outlet, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Footer from "./components/footer/Footer";
-import Header from "./components/header/Header";
 import Main from "./pages";
 
 function App() {
-  const MainLayout = () => {
-    return (
-      <>
-        <Header />
-        <Outlet />
-        <Footer />
-      </>
-    );
-  };
+  // const MainLayout = () => {
+  //   return (
+  //     <>
+  //       <Header />
+  //       <Outlet />
+  //       <Footer />
+  //     </>
+  //   );
+  // };
 
   return (
     <Routes>
-      <Route element={<MainLayout />}>
-        <Route path="/" element={<Main />} />
-      </Route>
+      <Route path="/" element={<Main />} />
     </Routes>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 import MainContents from "../components/MainContents";
 import MainSlide from "../components/MainSlide";
+import Footer from "../components/footer/Footer";
+import Header from "../components/header/Header";
 
 export default function Main() {
   return (
     <>
+      <Header />
       <MainSlide />
       <MainContents />
+      <Footer />
     </>
   );
 }
